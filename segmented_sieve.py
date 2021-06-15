4b93d75d210243d937063db56ca706ed985c3e42
Vikas Sanwal <vikassnwl@gmail.com>
2021-06-15 22:12:00 +0530

    Update segmented_sieve.py

--- segmented_sieve.py
+++ segmented_sieve.py
@@ -1,20 +1,28 @@
 # implementation of segmented sieve
- 
-primes = [2]
-for i in range(3, 32000, 2):
-    isPrime = True
-    cap = int(i**.5)
-    for j in primes:
-        if j > cap:
-            break
-        if i%j == 0:
-            isPrime = False
-            break
-    if isPrime:
-        primes.append(i)
- 
+
+MAX = 32000
+def sieve():
+    """
+    creating sieve of size 32001
+    """
+    isPrime = [1 for _ in range(MAX+1)]
+    print(len(isPrime))
+    for i in range(2, int(MAX**.5)+1):
+        for j in range(i*i, MAX+1, i):
+            if isPrime[j] == 1:
+                isPrime[j] = 0
+    primes = []
+    for i in range(2, MAX+1):
+        if isPrime[i] == 1:
+            primes.append(i)
+    print(len(primes))
+    return primes
+
+
+
 t = int(input())
 for _ in range(t):
+    primes = sieve()
     l, r = map(int, input().split())
     if l < 2:
         l = 2
