8b339d0fb9921b7efc3de83d42f16e6696f20f42
Vikas Sanwal <vikassnwl@gmail.com>
2021-06-18 19:39:13 +0530

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -15,16 +15,25 @@ class Solution:
       
 #solution 2 using bitmask O(Nx2^N)
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        output = []
-        for mask in range(1<<len(nums)):
-            result = []
-            for i in range(len(nums)):
-                if mask&(1<<i):
-                    result.append(nums[i])
-            output.append(result)
-            
-        return output
+def subsets(nums):
+    output = []
+    for mask in range(1<<len(nums)):
+        """
+        generating 2**n(where n is the len of nums) numbers
+        from 0 to 2**n-1, which will represent each subset
+        """
+        result = []
+        for i in range(len(nums)):
+            """
+            checking for set bit
+            and if bit is set then include the number
+            from nums to the result
+            """
+            if mask&(1<<i):
+                result.append(nums[i])
+        output.append(result)
+        
+    return output
 
 
 #solution 3 using built-in itertools module
